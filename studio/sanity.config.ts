bd2242bd5727bde9e9de8ff8a7847b0fa985220f
Joram Kroon <joramkroon@live.nl>
2025-12-08 17:18:18 +0100

    feat: add contact schema and singleton management to Sanity configuration

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -5,16 +5,31 @@ import {presentationTool} from 'sanity/presentation'
 
 import {schemaTypes} from './schemas'
 
+import {
+  filteredDocumentListItems,
+  singletonDocumentListItems,
+  singletonTools,
+} from 'sanity-plugin-singleton-management'
+
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
 const dataset = process.env.SANITY_STUDIO_DATASET!
 
 export default defineConfig({
   name: 'sanity-template-sveltekit-clean',
-  title: 'Clean SvelteKit + Sanity app',
+  title: 'CMS for Niko - Trust Your Mind',
   projectId,
   dataset,
   plugins: [
-    structureTool(),
+    structureTool({
+      structure: (S, context) =>
+        S.list()
+          .title('Singletons')
+          .items([
+            ...singletonDocumentListItems({S, context}),
+            S.divider(),
+            ...filteredDocumentListItems({S, context}),
+          ]),
+    }),
     presentationTool({
       previewUrl: {
         origin: process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:5173',
@@ -25,6 +40,7 @@ export default defineConfig({
       },
     }),
     visionTool(),
+    singletonTools(),
   ],
   schema: {
     types: schemaTypes,
