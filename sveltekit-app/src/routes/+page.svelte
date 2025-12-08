142e3ddbf29f7c2519ed4739126bcaa8c17b9427
Joram Kroon <joramkroon@live.nl>
2025-12-08 15:50:36 +0100

    chore: refactor layout and remove Welcome component; add outline toggle functionality

--- sveltekit-app/src/routes/+page.svelte
+++ sveltekit-app/src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { useQuery } from '@sanity/svelte-loader';
 	import Card from '../components/Card.svelte';
-	import Welcome from '../components/Welcome.svelte';
 	import type { PageData } from './$types';
+	import Hero from '../components/Hero.svelte';
+	import Usp from '../components/Usp.svelte';
 
 	export let data: PageData;
 	const q = useQuery(data);
@@ -11,11 +12,15 @@
 </script>
 
 <section>
-	{#if posts.length}
+	<!-- {#if posts.length}
 		{#each posts as post}
 			<Card {post} />
 		{/each}
 	{:else}
-		<Welcome />
-	{/if}
+		<p>Welkom op de home pagina van Niko</p>
+	{/if} -->
+
+	<Hero />
+
+	<Usp />
 </section>
