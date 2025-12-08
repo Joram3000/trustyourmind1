f0013f74e3069f7edd563802da97e250c659f242
Joram Kroon <joramkroon@live.nl>
2025-12-08 17:23:57 +0100

    refactor: remove unused Card component and clean up commented code in +page.svelte

--- sveltekit-app/src/routes/+page.svelte
+++ sveltekit-app/src/routes/+page.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
 	import { useQuery } from '@sanity/svelte-loader';
-	import Card from '../components/Card.svelte';
 	import type { PageData } from './$types';
+
+	import Services from '../components/Services.svelte';
+	import Contact from '../components/Contact.svelte';
 	import Hero from '../components/Hero.svelte';
 	import Usp from '../components/Usp.svelte';
-
 	export let data: PageData;
 	const q = useQuery(data);
 
-	$: ({ data: posts } = $q);
+	// $: ({ data: posts } = $q);
 </script>
 
 <section>
-	<!-- {#if posts.length}
-		{#each posts as post}
-			<Card {post} />
-		{/each}
-	{:else}
-		<p>Welkom op de home pagina van Niko</p>
-	{/if} -->
-
 	<Hero />
 
 	<Usp />
+
+	<Services />
+
+	<Contact />
 </section>
