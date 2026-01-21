5498e20d7f7ff50bbe2eb46fa278a762b8a8a016
Joram Kroon <joramkroon@live.nl>
2026-01-21 17:53:48 +0100

    fix: move autoUpdates configuration to the correct location in Sanity CLI config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -17,7 +17,7 @@ export default defineCliConfig({
   },
   deployment: {
     appId: process.env.SANITY_STUDIO_DEPLOYMENT_APP_ID || '',
+    autoUpdates: true,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
-  autoUpdates: true,
 })
